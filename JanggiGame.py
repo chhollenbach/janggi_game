81e3b2503f4252eb24fdb1105ff560122ef60fa1
chhollenbach <hollenbach256@gmail.com>
2021-02-26 23:06:45 -0600

    Added movement for all pieces except cannon and chariot. Improved display function. Next steps: finish chariot, cannon movement. finish make_move function. Work on logic for check/checkmate. improve docstrings/comments

--- JanggiGame.py
+++ JanggiGame.py
@@ -3,7 +3,7 @@
 # A program that implements Classes to play the board game "Janggi" aka Korean chess
 
 # Using external package to add color to terminal print in order to see red/blue side better
-# Remove/comment out before uploading to gradescope?
+# Will Remove/comment out before uploading to gradescope
 from termcolor import colored
 
 
@@ -36,17 +36,24 @@ class GameBoard:
         for key, value in sorted(self._board.items()):
             display_list.append(value)
 
-        # Print out list of lists vertically
+        # Print out list of lists vertically with column and row markers
+        print('  a  b  c  d  e  f  g  h  i')
         for col in range(10):
+            print(col + 1, end = ' ')
             for row in display_list:
-                print(row[col], end=' ')
+                print(row[col], end = ' ')
             print()
 
     def get_square(self, location):
         """Return a specific square on the board"""
         col = location[0]
         row = int(location[1:]) - 1
-        return self._board[col][row]
+        # if square is off board, return None
+        try:
+            return_square = self._board[col][row]
+        except (KeyError, IndexError):
+            return_square = None
+        return return_square
 
 
 class JanggiGame:
@@ -163,6 +170,8 @@ class JanggiGame:
         return True
 
 # TODO add return method for pieces that returns the diff of all squares able to move to
+
+
 class GamePiece:
     """Generic GamePiece class, tracks the team of a piece"""
     def __init__(self, team):
@@ -195,17 +204,20 @@ class General(GamePiece):
         move_list = []
         starting_col = location[0]
         starting_row = int(location[1:])
-        # Create initial 9 possible moves
-        for delta in (-1, 0, 1):
-            move_list.append(chr(ord(starting_col) + delta) + str(starting_row))
-            move_list.append(starting_col + str(starting_row + delta))
-            move_list.append(chr(ord(starting_col) + delta) + str(starting_row + delta))
-        # Filter out if square not in palace
-        for move in move_list:
-            if move not in gameboard.get_palace(self.get_team()):
-                move_list.remove(move)
+        palace_squares = gameboard.get_palace(self.get_team())
 
-        print(move_list)
+        # Create initial 9 possible moves
+        for row_delta in [-1, 0, 1]:
+            for col_delta in [-1, 0, 1]:
+                move_location = chr(ord(starting_col) + col_delta) + str(starting_row + row_delta)
+                # move must be in palace and square must be empty not occupied by enemy
+                if move_location in palace_squares and (
+                        gameboard.get_square(move_location) == '__' or
+                        gameboard.get_square(move_location).get_team() != self.get_team()):
+                    move_list.append(chr(ord(starting_col) + col_delta) + str(starting_row + row_delta))
+
+        return set(move_list)
+
 
 class Guard(GamePiece):
     """Guard Piece, can move 1 space in any direction within the palace"""
@@ -217,6 +229,25 @@ class Guard(GamePiece):
     def __str__(self):
         """Override print method to display gamepiece in terminal"""
         return colored("GD", self._team)
+
+    def valid_moves(self, location, gameboard):
+        """Determines valid moves for the piece, given the starting location, and the state of the gameboard"""
+        move_list = []
+        starting_col = location[0]
+        starting_row = int(location[1:])
+        palace_squares = gameboard.get_palace(self.get_team())
+
+        # Create initial 9 possible moves
+        for row_delta in [-1, 0, 1]:
+            for col_delta in [-1, 0, 1]:
+                move_location = chr(ord(starting_col) + col_delta) + str(starting_row + row_delta)
+                # move must be in palace and square must be empty not occupied by enemy
+                if move_location in palace_squares and (
+                        gameboard.get_square(move_location) == '__' or
+                        gameboard.get_square(move_location).get_team() != self.get_team()):
+                    move_list.append(chr(ord(starting_col) + col_delta) + str(starting_row + row_delta))
+
+        return set(move_list)
 
 
 class Horse(GamePiece):
@@ -233,9 +264,71 @@ class Horse(GamePiece):
         """Override print method to display gamepiece in terminal"""
         return colored("HS", self._team)
 
+    def valid_moves(self, location, gameboard):
+        """Determines valid moves for the piece, given the starting location, and the state of the gameboard"""
+        move_list = []
+        starting_col = location[0]
+        starting_row = int(location[1:])
+
+        # Create 8 possible moves
+        # Test if orthogonal square is empty, then test if diagonal square is empty or enemy occupied
+        # Right
+        if gameboard.get_square(chr(ord(starting_col) + 1) + str(starting_row)) == '__':
+            move_1 = chr(ord(starting_col) + 2) + str(starting_row + 1)
+            move_2 = chr(ord(starting_col) + 2) + str(starting_row - 1)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_1) == '__' or
+                    gameboard.get_square(move_1).get_team() != self.get_team()):
+                move_list.append(move_1)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_2) == '__' or
+                    gameboard.get_square(move_2).get_team() != self.get_team()):
+                move_list.append(move_2)
+
+        # Left
+        if gameboard.get_square(chr(ord(starting_col) - 1) + str(starting_row)) == '__':
+            move_1 = chr(ord(starting_col) - 2) + str(starting_row + 1)
+            move_2 = chr(ord(starting_col) - 2) + str(starting_row - 1)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_1) == '__' or
+                    gameboard.get_square(move_1).get_team() != self.get_team()):
+                move_list.append(move_1)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_2) == '__' or
+                    gameboard.get_square(move_2).get_team() != self.get_team()):
+                move_list.append(move_2)
+
+        # Up
+        if gameboard.get_square(chr(ord(starting_col)) + str(starting_row + 1)) == '__':
+            move_1 = chr(ord(starting_col) + 1) + str(starting_row + 2)
+            move_2 = chr(ord(starting_col) - 1) + str(starting_row + 2)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_1) == '__' or
+                    gameboard.get_square(move_1).get_team() != self.get_team()):
+                move_list.append(move_1)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_2) == '__' or
+                    gameboard.get_square(move_2).get_team() != self.get_team()):
+                move_list.append(move_2)
+
+        # Down
+        if gameboard.get_square(chr(ord(starting_col)) + str(starting_row - 1)) == '__':
+            move_1 = chr(ord(starting_col) + 1) + str(starting_row - 2)
+            move_2 = chr(ord(starting_col) - 1) + str(starting_row - 2)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_1) == '__' or
+                    gameboard.get_square(move_1).get_team() != self.get_team()):
+                move_list.append(move_1)
+            if gameboard.get_square(move_1) is not None and (
+                    gameboard.get_square(move_2) == '__' or
+                    gameboard.get_square(move_2).get_team() != self.get_team()):
+                move_list.append(move_2)
+
+        return set(move_list)
+
 
 class Elephant(GamePiece):
-    """Elephant piece, moves one step orthogonally and then two steps diagonally. Block by pieces, cannot jump."""
+    """Elephant piece, moves one step orthogonally and then two steps diagonally. Blocked by pieces, cannot jump."""
     def __init__(self, team):
         """Init Elephant with new type"""
         super().__init__(team)
@@ -244,6 +337,78 @@ class Elephant(GamePiece):
     def __str__(self):
         """Override print method to display gamepiece in terminal"""
         return colored("EL", self._team)
+
+    def valid_moves(self, location, gameboard):
+        """Determines valid moves for the piece, given the starting location, and the state of the gameboard"""
+        move_list = []
+        starting_col = location[0]
+        starting_row = int(location[1:])
+
+        # Create 8 possible moves
+        # Test if orthogonal square is empty, then test if diagonal square is empty or enemy occupied
+        # Nested if statements walk through movement to insure no piece is blocking path
+        # Movement is one step orthogonally, then one step diagonally out, then one more step diagonally out
+        # Right
+        if gameboard.get_square(chr(ord(starting_col) + 1) + str(starting_row)) == '__':
+            if gameboard.get_square(chr(ord(starting_col) + 2) + str(starting_row + 1)) == '__':
+                move = chr(ord(starting_col) + 3) + str(starting_row + 2)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+            if gameboard.get_square(chr(ord(starting_col) + 2) + str(starting_row - 1)) == '__':
+                move = chr(ord(starting_col) + 3) + str(starting_row - 2)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+
+        # Left
+        if gameboard.get_square(chr(ord(starting_col) - 1) + str(starting_row)) == '__':
+            if gameboard.get_square(chr(ord(starting_col) - 2) + str(starting_row + 1)) == '__':
+                move = chr(ord(starting_col) - 3) + str(starting_row + 2)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+            if gameboard.get_square(chr(ord(starting_col) - 2) + str(starting_row - 1)) == '__':
+                move = chr(ord(starting_col) - 3) + str(starting_row - 2)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+
+        # Up
+        if gameboard.get_square(chr(ord(starting_col)) + str(starting_row + 1)) == '__':
+            if gameboard.get_square(chr(ord(starting_col) + 1) + str(starting_row + 2)) == '__':
+                move = chr(ord(starting_col) + 2) + str(starting_row + 3)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+            if gameboard.get_square(chr(ord(starting_col) - 1) + str(starting_row + 2)) == '__':
+                move = chr(ord(starting_col) - 2) + str(starting_row - 3)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+
+        # Down
+        if gameboard.get_square(chr(ord(starting_col)) + str(starting_row - 1)) == '__':
+            if gameboard.get_square(chr(ord(starting_col) + 1) + str(starting_row - 2)) == '__':
+                move = chr(ord(starting_col) + 2) + str(starting_row - 3)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+            if gameboard.get_square(chr(ord(starting_col) - 1) + str(starting_row - 2)) == '__':
+                move = chr(ord(starting_col) - 2) + str(starting_row - 3)
+                if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                    move_list.append(move)
+
+        return set(move_list)
 
 
 class Chariot(GamePiece):
@@ -256,6 +421,10 @@ class Chariot(GamePiece):
     def __str__(self):
         """Override print method to display gamepiece in terminal"""
         return colored("CH", self._team)
+
+    def valid_moves(self, location, gameboard):
+        """Determines valid moves for the piece, given the starting location, and the state of the gameboard"""
+        pass
 
 
 class Cannon(GamePiece):
@@ -272,6 +441,10 @@ class Cannon(GamePiece):
         """Override print method to display gamepiece in terminal"""
         return colored("CA", self._team)
 
+    def valid_moves(self, location, gameboard):
+        """Determines valid moves for the piece, given the starting location, and the state of the gameboard"""
+        pass
+
 
 class Soldier(GamePiece):
     """
@@ -286,13 +459,38 @@ class Soldier(GamePiece):
         """Override print method to display gamepiece in terminal"""
         return colored("SD", self._team)
 
+    def valid_moves(self, location, gameboard):
+        """Determines valid moves for the piece, given the starting location, and the state of the gameboard"""
+        move_list = []
+        starting_col = location[0]
+        starting_row = int(location[1:])
+
+        # Create 3 possible moves, forward movement depending on team
+        move_sideways_1 = chr(ord(starting_col) - 1) + str(starting_row)
+        move_sidweays_2 = chr(ord(starting_col) + 1) + str(starting_row)
+        if self.get_team() == 'blue':
+            move_forward = chr(ord(starting_col)) + str(starting_row - 1)
+        else:
+            move_forward = chr(ord(starting_col)) + str(starting_row + 1)
+
+        # check that move is not off the board/onto a friendly piece
+        for move in [move_sideways_1, move_sidweays_2, move_forward]:
+            if gameboard.get_square(move) is not None and (
+                        gameboard.get_square(move) == '__' or
+                        gameboard.get_square(move).get_team() != self.get_team()):
+                move_list.append(move)
+
+        return set(move_list)
+
 
 def main():
     """Main function to test out code"""
     game = JanggiGame()
     game.get_janggi_board().display_board()
+    # Test movement
     board = game.get_janggi_board()
-    game.get_janggi_board().get_square('e2').valid_moves('e2',board)
+    print(game.get_janggi_board().get_square('a4').valid_moves('a4', board))
+
 
 if __name__ == '__main__':
     main()
